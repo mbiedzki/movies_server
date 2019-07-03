7f65d7e4e6511f0eb6ca1615e2bbb985efc6bb40
Michal Biedzki <45432236+mbiedzki@users.noreply.github.com>
2019-07-03 11:43:34 +0200

    Spring boot init

---
+++ src/main/java/pl/wasko/movies_server/MoviesServerApplication.java
@@ -0,0 +1,13 @@
+package pl.wasko.movies_server;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MoviesServerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MoviesServerApplication.class, args);
+	}
+
+}
